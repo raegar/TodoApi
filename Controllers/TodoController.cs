37810a8b5d152d1454220e10a5c1faaafc0ad74d
raegar <jamie.myland@gmail.com>
2019-04-15 22:00:43 +0100

    Add get methods

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -25,5 +25,26 @@ namespace TodoApi.Controllers
                 _context.SaveChanges();
             }
         }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        //GET: api/Todo/5
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(GetTodoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
     }
 }
